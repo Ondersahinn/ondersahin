24a82db6d3a46541945aa5b5f1a932a5cef9ded9
Önder Şahin <ondershin@gmail.com>
2022-09-07 22:48:52 +0300

    Add resources page

---
+++ pages/panel/resources/index.tsx
@@ -0,0 +1,72 @@
+import AddResources from '@components/forms/addBlog';
+import { adminCheckAuth } from '@utils/session';
+import { RootState } from "@redux/reducers";
+import { changeCategoriesStatus, fetchCategories } from "@redux/slices/categories";
+import { Table } from "antd";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { http } from "src/api/http";
+
+export const Resources: React.FC = () => {
+    const colums = [
+        {
+            title: 'Category Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Path',
+            dataIndex: 'path',
+            key: 'path',
+        },
+        {
+            title: 'Create date time',
+            dataIndex: 'since',
+            key: 'since',
+        },
+        {
+            title: 'Action',
+            dataIndex: '',
+            key: 'x',
+            render: (text: any, record: any, index: number) => <a onClick={() => handleDelete(record)}>Delete</a>,
+        }
+    ];
+
+    const [isList, setIsList] = useState<boolean>(true);
+    const status = useSelector((state: RootState) => state.categories.status);
+    const categories = useSelector((state: RootState) => state.categories.categories);
+    const dispatch = useDispatch();
+
+
+    const handleDelete = async (record: any) => {
+        await http.delete('/api/categories?id=' + record.key).then((res)=> {
+            dispatch(changeCategoriesStatus('idle'))
+        })
+    }
+
+    useEffect(() => {
+        if (status === 'idle') {
+            dispatch(fetchCategories())
+        }
+    }, [dispatch, status])
+
+    return (
+        <>
+            <div>
+                <div className="flex justify-end">
+                    <button type="button" onClick={() => setIsList(!isList)}
+                        className="py-2.5 float-right px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">{isList ? 'Kategori Ekle' : 'Listeye Dön'}</button>
+                </div>
+                {isList ?
+                    <Table dataSource={categories.map((x: any) => { return { ...x, key: x._id } })} columns={colums} />
+                    : <AddResources />}
+            </div>
+        </>
+    )
+}
+
+
+export default Resources;
+Resources.displayName = "PanelPage";
+
+export const getServerSideProps = adminCheckAuth({});
