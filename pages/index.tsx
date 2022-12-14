ce5b090ab6229194bd72dcab0b5a9299d63fc983
Önder Şahin <ondershin@gmail.com>
2022-12-14 15:45:00 +0300

    footer link and seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import Hero from "@sections/hero";
+import Head from "next/head";
 
-export default function Home(props : any) {
+export default function Home(props: any) {
   return (
-    <div>
-      <Hero />
-    </div>
+    <>
+      <Head>
+        <title>Önder Şahin - Anasayfa</title>
+        <meta charSet="utf-8" />
+         <title>Önder Şahin</title>
+         <meta name="title" content='Önder Şahin' />
+         <meta name="description" content='Önder Şahin - Portfolyo & Blog' />
+         <meta name="keywords" content='reactjs, önder, önder şahin, js,'/>
+         <meta property="og:title" content='Önder Şahin' />
+         <meta property="og:description" content='Önder Şahin - Portfolyo & Blog'/>
+         <meta property="og:url" content='ondersahin.com.tr' />
+      </Head>
+      <div>
+        <Hero />
+      </div>
+    </>
   )
 }
--- sections/footer.tsx
+++ sections/footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <footer className="py-6 text-center text-gray-500 text-sm">
-      <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2"> Önder Şahin </span>&copy; {new Date().getFullYear()} Bu web sitesinin kaynak kodlarına Github üzerinden ulaşabilirsiniz.
+     <a href="https://github.com/Ondersahinn/ondersahin" target='_blank' rel="noreferrer"> <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2"> Önder Şahin </span>&copy; {new Date().getFullYear()} Bu web sitesinin kaynak kodlarına Github üzerinden ulaşabilirsiniz.</a>
     </footer>
   )
 }
